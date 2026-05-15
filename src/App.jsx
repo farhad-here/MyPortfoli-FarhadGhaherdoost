fc66a16016c1afb5fc1002562518b5969765546f
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-05-15 20:03:56 +0330

    Initial contactme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from './components/Header.jsx';
 import {Home} from './Pages/Home.jsx';
 import { AboutMe } from "./Pages/AboutMe.jsx";
+import { ContactMe } from "./Pages/ContactMe.jsx";
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/About" element={<AboutMe />}></Route>
+        <Route path="/Contact" element={<ContactMe />}></Route>
 
 
       </Routes>
