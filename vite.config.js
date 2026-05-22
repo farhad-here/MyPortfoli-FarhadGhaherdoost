626cdb1ee1b692bbb72a3fafed2dc995e002cd19
Farhad_Ghaherdoost <farhadghaher@gmail.com>
2026-05-22 20:12:49 +0330

    Fix Bug for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: 'MyPortfoli-FarhadGhaherdoost',
-  plugins: [react()],
-  build: {
-    outDir: "docs"
-  }
-})
+    export default defineConfig({
+      plugins: [react()],
+      base: process.env.NODE_ENV === 'production' && process.env.VERCEL === '1' ? './' : 'MyPortfoli-FarhadGhaherdoost',
+      build: {
+        outDir: process.env.NODE_ENV === 'production' && process.env.VERCEL === '1' ? 'dist' : 'docs',
+      }
+    })
